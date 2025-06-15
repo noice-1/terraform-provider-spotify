016b21ead69cc4d26c3da5180b9d89bf66b21da3
noice-1 <hamzanisarahamed@gmail.com>
2025-06-15 22:47:35 +0530

    add tracks

--- playlist.tf
+++ playlist.tf
@@ -1,4 +1,8 @@
+data "spotify_search_track" "acdc" {
+  artist = "AC/DC"
+}
+
 resource "spotify_playlist" "tf_project" {
   name = "tf project"
-  tracks = ["4OROzZUy6gOWN4UGQVaZMF"]
+  tracks = ["4OROzZUy6gOWN4UGQVaZMF",data.spotify_search_track.acdc.tracks[0].id]
 }
