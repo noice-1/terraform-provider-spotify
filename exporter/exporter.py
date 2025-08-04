97273cb7cf79101e994e4323479a8df7a4b984c2
noice-1 <hamzanisarahamed@gmail.com>
2025-08-05 00:42:21 +0530

    exporter setup

---
+++ exporter/exporter.py
@@ -0,0 +1,15 @@
+from prometheus_client import start_http_server, Counter
+import time
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+spotify_auth_requests = Counter("spotify_auth_requests_total", "Total Spotify Auth Requests")
+
+if __name__ == "__main__":
+    start_http_server(8000)  # Port Prometheus will scrape
+    while True:
+        # Dummy increment for example
+        spotify_auth_requests.inc()
+        logging.info("Incremented spotify_auth_requests")
+        time.sleep(10)
